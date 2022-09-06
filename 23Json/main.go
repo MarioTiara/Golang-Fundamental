f05b66fc54de1683388c54eb7e7be8867e6f019e
MarioTiara <mario.tiarapratama@gmail.com>
2022-09-06 21:31:29 +0700

    decode json

--- 23Json/main.go
+++ 23Json/main.go
@@ -15,7 +15,8 @@ type course struct {
 
 func main() {
 	fmt.Println("Json in Go")
-	EncodeJson()
+	//EncodeJson()
+	DecodeJson()
 }
 
 func EncodeJson() {
@@ -32,4 +33,36 @@ func EncodeJson() {
 	}
 
 	fmt.Printf("%s\n", finalJson)
+}
+
+func DecodeJson() {
+	jsonDataFromWeb := []byte(`
+		{
+			"coursename":"Golang Bootcamp",
+			"Price":299,
+			"webise":"mario.pratama.com",
+			"tags":["web-dev", "js"]
+		}
+	`)
+
+	var lcoCourses course
+
+	checkvalid := json.Valid(jsonDataFromWeb)
+	if checkvalid {
+		fmt.Println("Json was valid")
+		json.Unmarshal(jsonDataFromWeb, &lcoCourses)
+		fmt.Printf("%#v\n", lcoCourses)
+	} else {
+		fmt.Println("Json was not valid")
+	}
+
+	// some case where you just want to addd data key value
+	var myOnlineData map[string]interface{}
+	json.Unmarshal(jsonDataFromWeb, &myOnlineData)
+	fmt.Printf("%#v\n", myOnlineData)
+
+	for k, v := range myOnlineData {
+		fmt.Printf("ker is %v and value is %v and type is : %T\n", k, v, v)
+	}
+
 }
