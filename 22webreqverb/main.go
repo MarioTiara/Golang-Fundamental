33e2c4ec3a409c0054ccbfa26b1c19e99e1969af
XTR\mario.pratama <mario.pratama@xtremax.com>
2022-09-06 19:40:45 +0700

    Post req json in go

--- 22webreqverb/main.go
+++ 22webreqverb/main.go
@@ -9,7 +9,27 @@ import (
 
 func main() {
 	fmt.Println("Welcome to get request with go")
-	PerformGetRequest()
+	//PerformGetRequest()
+	PerformPostJsonRequest()
+}
+
+func PerformPostJsonRequest() {
+	const myurl = "http://localhost:8000/post"
+	//fake json pyload
+	requestBody := strings.NewReader(`
+	{
+		"coursename":"Let's go with golang",
+		"price": 0,
+		"platform":"learnCodeOnline.in"
+	}`)
+
+	response, err := http.Post(myurl, "application/json", requestBody)
+	if err != nil {
+		panic(err)
+	}
+	defer response.Body.Close()
+	content, _ := ioutil.ReadAll(response.Body)
+	fmt.Println(string(content))
 }
 
 func PerformGetRequest() {
