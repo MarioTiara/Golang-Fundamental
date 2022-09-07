37d70bd3313b29ef6d89fc97163349613f269109
MarioTiara <mario.tiarapratama@gmail.com>
2022-09-08 06:47:12 +0700

    get one course func

--- 25buildapi/main.go
+++ 25buildapi/main.go
@@ -4,9 +4,11 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
-//Model for course - file
+// Model for course - file
 type Course struct {
 	CourseId    string  `json:"courseid"`
 	CourseName  string  `json:"coursename"`
@@ -30,16 +32,35 @@ func main() {
 
 }
 
-
 //controller - file
 
-//server home route
-func serveHome (w http.ResponseWriter, r *http.Request){
-	w.Write([] byte("<h1> Welcome to first mario API using Golang"))
+// server home route
+func serveHome(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("<h1> Welcome to first mario API using Golang"))
 }
 
-func getAllCourse (w http.ResponseWriter, r *http.Request){
+func getAllCourse(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Get all course")
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(courses)
+}
+
+func getOneCourse(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("GetCourse")
+	w.Header().Set("Content-Type", "applicaion/json")
+
+	//garb id from request
+	params:=mux.Vars(r)
+
+	//loop through courses, find matching id and return the response
+	for _, course :=range courses {
+		if course.CourseId ==params["id"]{
+			json.NewEncoder(w).Encode(course)
+			return 
+		}
+	}
+
+	json.NewEncoder(w).Encode("No Course found with given id")
+	return
+
 }
