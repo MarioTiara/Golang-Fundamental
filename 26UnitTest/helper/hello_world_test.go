684684cbbb66d92de9df05bd098aedb4275bb4d2
MarioTiara <mario.tiarapratama@gmail.com>
2022-09-25 10:42:14 +0700

    [Unit Testing] Unit testing basic

--- 26UnitTest/helper/hello_world_test.go
+++ 26UnitTest/helper/hello_world_test.go
@@ -2,8 +2,18 @@ package helper
 
 import (
 	"fmt"
+	"runtime"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
+
+func TestMain(m *testing.M) {
+	fmt.Println("Before Unit Test")
+	m.Run()
+	fmt.Println("After Unit Test")
+}
 
 func TestHelloWorld(t *testing.T) {
 	expected := "Hello mario"
@@ -17,10 +27,82 @@ func TestHelloWorld(t *testing.T) {
 
 func TestHelloWorldPratama(t *testing.T) {
 	expected := "Hello pratama"
-	result := HelloWorld("putra")
+	result := HelloWorld("pratama")
 	if result != expected {
 		errmessage := fmt.Sprintf("\nresult: %s \nexpected: %s", result, expected)
 		t.Error(errmessage)
 		//t.Fail()
 	}
+}
+
+func TestHelloWorRequire(t *testing.T) {
+	expected := "Hello pratama"
+	result := HelloWorld("pratama")
+	errmessage := fmt.Sprintf("result must be '%s'", expected)
+	require.Equal(t, expected, result, errmessage)
+}
+
+func TestHelloWorldAssert(t *testing.T) {
+	expected := "Hello pratama"
+	result := HelloWorld("pratama")
+	errmessage := fmt.Sprintf("result must be '%s'", expected)
+	assert.Equal(t, expected, result, errmessage)
+}
+
+func TestSkip(t *testing.T) {
+	if runtime.GOOS == "windows" {
+		t.Skip("Unit test can't running on windows")
+	}
+	expected := "Hello pratama"
+	result := HelloWorld("pratama")
+	errmessage := fmt.Sprintf("result must be '%s'", expected)
+	assert.Equal(t, expected, result, errmessage)
+}
+
+func TestSubTest(t *testing.T) {
+	t.Run("Mario", func(t *testing.T) {
+		expected := "Hello Mario"
+		result := HelloWorld("Mario")
+		errmessage := fmt.Sprintf("result must be '%s'", expected)
+		assert.Equal(t, expected, result, errmessage)
+	})
+
+	t.Run("Pratama", func(t *testing.T) {
+		expected := "Hello Pratama"
+		result := HelloWorld("Pratama")
+		errmessage := fmt.Sprintf("result must be '%s'", expected)
+		assert.Equal(t, expected, result, errmessage)
+	})
+}
+
+func TestTableHelloworld(t *testing.T) {
+	tests := []struct {
+		name     string
+		request  string
+		expected string
+	}{
+		{
+			name:     "Mario",
+			request:  "Mario",
+			expected: "Hello Mario",
+		},
+		{
+			name:     "Pratama",
+			request:  "Pratama",
+			expected: "Hello Pratama",
+		},
+		{
+			name:     "Roby",
+			request:  "Roby",
+			expected: "Hello Roby",
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			result := HelloWorld(test.request)
+			assert.Equal(t, test.expected, result)
+		})
+	}
+
 }
